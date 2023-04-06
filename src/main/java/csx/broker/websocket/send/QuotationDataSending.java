a16b572607cd4002012f050fc9879e733e84535e
Khoeung-Sokhai <sokhaikhoeung@gmail.com>
2023-04-06 11:06:16 +0700

    updateV2

--- src/main/java/csx/broker/websocket/send/QuotationDataSending.java
+++ src/main/java/csx/broker/websocket/send/QuotationDataSending.java
@@ -15,33 +15,28 @@ public class QuotationDataSending {
 
     public void sending(RawSocketHandler rawSocketHandler, Broker broker) {
 
-
-//        String msgSend = broker.getBrokerId() + broker.getIssueCode() + broker.getOrderDate().SimpleDateFormat("yyyyMMdd");
-
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         String formattedDate = sdf.format(broker.getOrderDate());
 
         if (broker.getOrderType() == 1) {
-            String msgSend =    "A1"                                            +
-                                format("%09d",broker.getIssueCode())           +
-//                                format("%04d",broker.getBrokerId())             +
-//                                format("%05d",broker.getAccountNo())            +
-                                format("%08d",broker.getOriginalOrderNo())      +
-                                format("%08d", broker.getOrderUV())             +
-                                format("%08d", broker.getOrderUV())             + formattedDate;
-
+            String msgSend =    "A1"                                                      +
+                                format("%09d",broker.getIssueCode())                      +
+                                format("%04d",Integer.parseInt(broker.getBrokerId()))     +
+                                format("%04d",Integer.parseInt(broker.getAccountNo()))    +
+                                format("%08d",broker.getOriginalOrderNo())                +
+                                format("%08d", broker.getOrderUV())                       +
+                                format("%08d", broker.getOrderUV())                       + formattedDate;
             log.info("Send growth board market data [{}]", msgSend); //console.log in intellij
             rawSocketHandler.sendAll(msgSend);
         }
         if (broker.getOrderType() == 2) {
-            String msgSend =    "A2"                                            +
-                                format("%09d",broker.getIssueCode())           +
-                                format("%A8s",broker.getBrokerId())             +
-                                format("%s",broker.getAccountNo())            +
-                                format("%08d",broker.getOriginalOrderNo())      +
-                                format("%08d", broker.getOrderUV())             +
-                                format("%08d", broker.getOrderUV())             + formattedDate;
-
+            String msgSend =    "A2"                                                      +
+                                format("%09d",broker.getIssueCode())                      +
+                                format("%04d",Integer.parseInt(broker.getBrokerId()))     +
+                                format("%04d",Integer.parseInt(broker.getAccountNo()))    +
+                                format("%08d",broker.getOriginalOrderNo())                +
+                                format("%08d", broker.getOrderUV())                       +
+                                format("%08d", broker.getOrderUV())                       + formattedDate;
             log.info("Send growth board market data [{}]", msgSend); //console.log in intellij
             rawSocketHandler.sendAll(msgSend);
         }
